44157a5a927e6ba2753947ac187c10ce42ad0d51
kpkusto <kpkusto@proton.me>
2026-06-02 22:44:43 -0300

    1ra version - ProyectoFinal

---
+++ inicio.py
@@ -0,0 +1,32 @@
+def menu_inicio():
+    print("                                                          ")
+    print("                                                          ")
+    print("         ------------Bienvenido al sistema IHKP-----------")
+    print("         |                                               |")
+    print("         |  1.Ingrese a la opcion 1                      |")
+    print("         |                                               |")
+    print("         |  2.Ingrese a la opcion 2                      |")
+    print("         |                                               |")
+    print("         |  3.Ingrese a la opcion 3                      |")
+    print("         |                                               |")
+    print("         |  4.Salir                                      |")
+    print("         |                                               |")
+    print("         -----Desarrollado por Santiago Dellapiazza-------")
+    print("                                                          ")
+    print("                                                          ")
+
+while True:
+    menu_inicio()
+    opcion = int(input("          Seleccione una opcion ( 1-4 )   "))
+    match opcion:
+        case 1:
+            print("Ingreso a la opcion 1")
+        case 2:
+            print("Ingreso a la opcion 2")
+        case 3:
+            print("Ingreso a la opcion 3")
+        case 4:
+            print("             Saliendo del programa...")
+            break
+        case _:
+            print(" Opcion invalida. Intente nuevamente.")
