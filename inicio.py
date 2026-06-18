208554b64a47f80d1cdc0f07c0970c703e3e1998
kpkusto <kpkusto@proton.me>
2026-06-18 11:57:52 -0300

    Modificaciones en el inicio.py y logs.txt

--- inicio.py
+++ inicio.py
@@ -1,3 +1,7 @@
+
+#PROGRAMA IHKP HECHO POR SANTIAGO DELLAPIAZZA. HECHO EN EL MES DE JUNIO DEL 2026 PARA EL INSTITUTO TECNOLOGICO DE INFORMATICA (I.T.I.)
+
+
 def menu_inicio():
     print("                                                                                                                                  ")
     print("                                                                                                                                  ")
@@ -26,31 +30,39 @@ def menu_inicio():
     print("                                                                                                                                  ")
     print("                                     ------------Bienvenido al sistema IHKP-----------                                            ")
     print("                                     |                                               |                                            ")
-    print("                                     |  1.Ingrese a la opcion 1                      |                                            ")
+    print("                                     |  1.Cargar logs                                |                                            ")
     print("                                     |                                               |                                            ")
-    print("                                     |  2.Ingrese a la opcion 2                      |                                            ")
+    print("                                     |  2.Ver estadisticas                           |                                            ")
     print("                                     |                                               |                                            ")
-    print("                                     |  3.Ingrese a la opcion 3                      |                                            ")
+    print("                                     |  3.Detectar IPs sospechosas                   |                                            ")
     print("                                     |                                               |                                            ")
     print("                                     |  4.Salir                                      |                                            ")
     print("                                     |                                               |                                            ")
     print("                                     -----Desarrollado por Santiago Dellapiazza-------                                            ")
     print("                                                                                                                                  ")
     print("                                                                                                                                  ")
+    print("                                                                                                                                  ")
+    print("                                                                                                                                  ")
+    print("                                                                                                                                  ")
+    print("                                                                                                                                  ")
+    print("                                                                                                                                  ")
+    print("                                                                                                                                  ")
+    print("                                                                                                                                  ")
 
 while True:
+
+    #meter un try + except
     menu_inicio()
     opcion = int(input("          Seleccione una opcion ( 1-4 )   "))
     match opcion:
         case 1:
-            print("Ingreso a la opcion 1")
+            print("                 Ingreso a la opcion 1   ")
         case 2:
-            print("Ingreso a la opcion 2")
+            print("                 Ingreso a la opcion 2   ")
         case 3:
-            print("Ingreso a la opcion 3")
+            print("                 Ingreso a la opcion 3   ")
         case 4:
-            print("             Saliendo del programa...        ")
+            print("                  Saliendo del programa...        ")
             break
         case _:
-            print(" Opcion invalida. Intente nuevamente.")
-
+            print("             Opcion invalida. Intente nuevamente.    ")
